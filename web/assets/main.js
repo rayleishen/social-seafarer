a67d0eb99f550ae4b111c4c07a337ffac64443e7
caleb05w <caleb05w@gmail.com>
2023-08-05 19:10:32 -0700

    Changed configurations tab

--- web/assets/main.js
+++ web/assets/main.js
@@ -57,5 +57,17 @@ window.onclick = function(event) {
     }
   }
 }
-    
+
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginRight = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
+}
     
