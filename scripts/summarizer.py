b0f0d5f3b408fa21512948616e1bb07073c615d1
rickyzhu2004 <73552739+rickyzhu2004@users.noreply.github.com>
2023-08-10 19:07:42 -0700

    Trying to make the summarizer
    The output has some errors, such as outputting random characters at the top, but the paragraph is generally working.

--- scripts/summarizer.py
+++ scripts/summarizer.py
@@ -10,14 +10,8 @@ import nltk
 #nltk.download('stopwords')
 
 import re
-
-#with open('config.json') as config_file:
-#    data = json.load(config_file)
-
-#key = data['openai_key']
-
-#openai.api_key = key
-
+import os
+import math
 
 
 def keywords(site, sens_rake):
@@ -45,6 +39,70 @@ def keywords(site, sens_rake):
     for rating, keyword in r.get_ranked_phrases_with_scores():
         if rating > sens_rake:
             #print(rating, keyword)
-            keywords_file.write(str(rating) + " " + keyword + "\n")
+            
+            #rating = int(rating*100)/100
+            #keywords_file.write(str(rating) + " " + keyword + "\n")
+            keywords_file.write(keyword + "\n")
             
     keywords_file.close()
+
+
+
+def compile_summary():
+    with open('json/config.json') as config_file:
+        data = json.load(config_file)
+
+    key = data['openai_key']
+
+    openai.api_key = key
+
+    # Define file paths
+    input_file_path = 'dynamic_products/keywords.txt'
+    output_file_path = 'dynamic_products/summary.txt'
+
+    # Read keywords and weights from the input file
+    with open(input_file_path, 'r') as input_file:
+        lines = input_file.readlines()
+
+    # Parse the lines into weights and messages
+    data = []
+    total_tokens = 0  # To keep track of the total tokens
+    for line in lines:
+        message = line.strip()
+        message_tokens = len(message.split())  # Calculate the tokens in the message
+        if total_tokens + message_tokens + 1 <= 3750:  # Check if adding the line exceeds the limit
+            data.append(message)  # Only store the message content
+            total_tokens += message_tokens + 1  # Add 2 for the space
+
+    # Generate a summary using OpenAI
+    keywords = '\n'.join(data)
+    prompt = f'Summarize the general sentiment of the following keywords into one cohesive paragraph:\n{keywords}'
+    
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=250  # You can adjust this value to control the length of the summary
+    )
+    summary = response.choices[0].text.strip()  # Remove leading/trailing whitespace
+    
+    newline_index = summary.find('\n')
+
+    # Extract the portion of text after the newline
+    extracted_text = summary[newline_index + 1:]
+    # print(extracted_text) For debugging purposes
+
+    # Create the folder if it doesn't exist
+    folder_path = os.path.dirname(output_file_path)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    # Write the summary to the output file
+    with open(output_file_path, 'w') as output_file:
+        output_file.write(extracted_text)
+
+    print(f"Summary saved to '{output_file_path}'")
+
+    # Rest of your existing code follows...
+
+# Call the function to compile the summary
+compile_summary()
